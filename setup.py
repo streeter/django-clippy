8a1c07801c4c5c0b8a3980e97e3b0901b9342f25
Chris Streeter <chris@chrisstreeter.com>
2012-06-07 19:01:53 -0700

    Include package data.

--- setup.py
+++ setup.py
@@ -3,22 +3,24 @@ from setuptools import setup, find_packages
 
 from clippy import VERSION
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
+
 requirements = []
 
 setup(
-    name = "django-clippy",
-    version = ".".join(map(str, VERSION)),
-    description = "A template tag for a 'copy to clipboard' button, based on GitHub's Clippy.",
-    long_description = read('README.rst'),
-    url = '',
-    license = 'MIT',
-    author = 'Flavio Curella',
-    author_email = 'flavio.curella@curella.org',
-    packages = find_packages(exclude=['tests']),
-    classifiers = [
+    name="django-clippy",
+    version=".".join(map(str, VERSION)),
+    description="A template tag for a 'copy to clipboard' button, based on GitHub's Clippy.",
+    long_description=read('README.rst'),
+    include_package_data=True,
+    url='',
+    license='MIT',
+    author='Flavio Curella',
+    author_email='flavio.curella@curella.org',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
@@ -28,7 +30,7 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    install_requires = requirements,
-    tests_require = ["nose",],
-    test_suite = "nose.collector",
+    install_requires=requirements,
+    tests_require=["nose", ],
+    test_suite="nose.collector",
 )
